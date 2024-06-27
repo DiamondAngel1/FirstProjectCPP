09246fab8d5d15f18ec6f85dbbbfb596dff5652b
DiamondAngel1 <tararukolga78@gmail.com>
2024-06-27 17:28:33 +0300

    Add function Init

--- Project1/Project1/FileName.cpp
+++ Project1/Project1/FileName.cpp
@@ -4,7 +4,12 @@ using namespace std;
 int Sum(int x, int y) {
 	return x + y;
 }
+void Init(int& x, int& y) {
+	cin >> x >> y;
+}
+
 int main() {
-	int x = 10, y = 25;
+	int x , y ;
+	Init(x, y);
 	cout << Sum(x, y) << endl;
 }
